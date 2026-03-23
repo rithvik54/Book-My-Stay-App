492df9ec642116d3e3c29303405377c1d9447c30
E Rithvik <rithvikrithvik54@gmail.com>
2026-03-24 01:20:17 +0530

    Update UseCase1HotelBookingApp.java

--- src/UseCase1HotelBookingApp.java
+++ src/UseCase1HotelBookingApp.java
@@ -1,13 +1,10 @@
-public class UseCase1HotelBookingApp {
+public class UseCase2RoomCreationApp {
 
     public static void main(String[] args) {
 
-        System.out.println("======================================");
-        System.out.println("     WELCOME TO BOOK MY STAY APP       ");
-        System.out.println("======================================");
+        StandardRoom room = new StandardRoom();
 
-        System.out.println("Hotel Booking Management System Started Successfully 👍");
-
-        System.out.println("Version : 1.0");
+        room.roomType();
+        room.displayRoomDetails();
     }
 }
